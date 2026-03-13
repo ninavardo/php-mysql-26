36e393f7403360e36035c06ff69f5960e5b38256
ninavardo <ninavardo5gmail.com>
2026-03-13 22:24:30 +0400

    task from practical_2

---
+++ homework/practical_2/task_3.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task_2</title>
+</head>
+<body></body>
+<?php
+
+$matrix = [];
+
+for($i = 0; $i < 6; $i++){
+    for($j = 0; $j < 5; $j++){
+        $matrix[$i][$j] = $i + $j;
+    }
+}
+
+echo "<table border='1'>";
+
+for($i = 0; $i < 6; $i++){
+    echo "<tr>";
+
+    for($j = 0; $j < 5; $j++){
+        echo "<td>".$matrix[$i][$j]."</td>";
+    }
+
+    echo "</tr>";
+}
+
+echo "</table>";
+
+?>
+
+</body>
+</html>
